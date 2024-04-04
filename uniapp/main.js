5e45dcee645eeaae7ce03b2c22ccb89ad0063316
gallantsa <gallantsa@163.com>
2024-04-04 21:04:48 +0800

    (小程序)开发个人信息功能

--- uniapp/main.js
+++ uniapp/main.js
@@ -5,23 +5,28 @@ import Vue from 'vue'
 import './uni.promisify.adaptor'
 import '@/static/css/global.css'
 import request from '@/utils/request.js'
+import apiConfig from '@/config.js'
+const baseUrl = process.env.NODE_ENV === "development" ? apiConfig.dev.baseUrl : apiConfig.prod.baseUrl
 
 Vue.config.productionTip = false
 Vue.prototype.$request = request
+Vue.prototype.$baseUrl = baseUrl
 
 App.mpType = 'app'
 const app = new Vue({
-  ...App
+	...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+	createSSRApp
+} from 'vue'
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	return {
+		app
+	}
 }
 // #endif
